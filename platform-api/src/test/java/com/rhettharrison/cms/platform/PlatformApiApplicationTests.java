1f33e0ce7c700eca46d31d9b71e3ac959f9b3414
Rhett Harrison <reharri7@asu.edu>
2026-01-20 16:35:00 -0800

    Initialize platform-api project with Spring Boot, Maven Wrapper, and initial configurations

---
+++ platform-api/src/test/java/com/rhettharrison/cms/platform/PlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhettharrison.cms.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
